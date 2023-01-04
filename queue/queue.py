68d6b817919135e72b48ed39c53d959a354a10c0
Lucas Rangel <lucasfijf@gmail.com>
2023-01-04 17:25:16 -0300

    refactor queue

--- queue/queue.py
+++ queue/queue.py
@@ -2,58 +2,56 @@ from node import Node
 
 class Queue:
     def __init__(self):
+        self.first = None
         self.last = None
-
-    # Verifica se a fila está vazia
-    def check_empty(self):
-        return self.last is None
+        self._size = 0
 
     # Adiciona um novo elemento à fila
     def push(self, data):
         node = Node(data)
-        node.next = self.last
-        self.last = node
+        if self.last is None:
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+        if self.first is None:
+            self.first = node
+        self._size += 1
 
     # Retorna o primeiro da fila sem removê-lo
     def peek(self):
-        if not self.check_empty():
-            first = self.last
-            while True:
-                if first.next != None:
-                    first = first.next
-                else:
-                    break
-            return first.data
+        if self._size > 0:
+            node = self.first.data
+            return node
         raise IndexError("The queue is empty")
 
     # Remove o primeiro elemento e o retorna
     def pop(self):
-        if not self.check_empty():
-            first = self.last
-            node = self.last
-            while True:
-                if first.next != None:
-                    node = first
-                    first = first.next
-                else:
-                    node.next = None
-                    break
-            return first.data
+        if self._size > 0:
+            node = self.first.data
+            self.first = self.first.next
+            if self.first is None:
+                self.last = None
+            self._size -= 1
+            return node
         raise IndexError("The queue is empty")
+
+    # Tamanho da fila
+    def __len__(self):
+        return self._size
 
     # Representação da fila
     def __repr__(self):
-        string = ""
-        node = self.last
-        if self.last != None:
-            while True:
-                string += "[" + str(node.data) + "] -> "
-                if node.next != None:
-                    node = node.next
-                else:
+        if self._size > 0:
+            string = ""
+            node = self.first
+            while node:
+                string += "[ " + str(node.data) + " ]->"
+                node = node.next
+                if node is None:
                     string += "None"
-                    break
-        return string
+            return string
+        raise IndexError("The queue is empty")
 
     # Print do objeto
     def __str__(self):
